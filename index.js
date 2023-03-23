1f413aa1de119f65b56cd295a11b89b99081bb88
sadmanador <sadman.ador@gmail.com>
2023-03-23 18:12:43 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config()
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+res.send('The Web is running')
+});
+
+
+app.listen(port, () => {
+console.log(`listening port ${port}`);
+});
