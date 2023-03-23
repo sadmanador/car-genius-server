482139dfeea69394b77e7ab2c9931c06d2c3076d
sadmanador <sadman.ador@gmail.com>
2023-03-23 18:35:38 +0600

    67-2 done!

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ app.use(cors());
 app.use(express.json());
 
 
+//mongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://car-genius:SovAGND5bOUVHLDY@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+    const servicesCollection = client.db('car-genius').collection('services');
+    app.post('/services', async (req, res) => {
+    const services = req.body;
+    const result = await servicesCollection.insertOne(services);
+    res.send(result);
+    });
+
+    app.get('/services', async (req, res) => {
+    const query = {};
+    const cursor = servicesCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+    });
+}
+finally{
+}
+};
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
 res.send('The Web is running')
 });
