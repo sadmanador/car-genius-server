7b7ffd0650e2eb8c9e73aec422679ef95361e317
sadmanador <sadman.ador@gmail.com>
2023-03-23 23:32:50 +0600

    67-3 done

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-require('dotenv').config()
-
+const cors = require("cors");
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
 //mongoDB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://car-genius:SovAGND5bOUVHLDY@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-try{
-    const servicesCollection = client.db('car-genius').collection('services');
-    app.post('/services', async (req, res) => {
-    const services = req.body;
-    const result = await servicesCollection.insertOne(services);
-    res.send(result);
+const uri =
+  "mongodb+srv://car-genius:SovAGND5bOUVHLDY@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const servicesCollection = client.db("car-genius").collection("services");
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.send(result);
     });
 
-    app.get('/services', async (req, res) => {
-    const query = {};
-    const cursor = servicesCollection.find(query);
-    const result = await cursor.toArray();
-    res.send(result);
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+    //   const query = {_id: id}
+      const result = await servicesCollection.findOne({_id: ObjectId(id)});
+      res.send(result);
+    });
+  } finally {
+  }
 }
-finally{
-}
-};
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-res.send('The Web is running')
+app.get("/", (req, res) => {
+  res.send("The Web is running");
 });
 
-
 app.listen(port, () => {
-console.log(`listening port ${port}`);
+  console.log(`listening port ${port}`);
 });
