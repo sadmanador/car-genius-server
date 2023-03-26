00e4c6bbc777075677d988fb0db773d197ce3eb1
sadmanador <sadman.ador@gmail.com>
2023-03-26 15:21:07 +0600

    67-8 done!

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ async function run() {
       const result = await ordersCollection.insertOne(orders);
       res.send(result);
     });
+
+    app.delete('/orders/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await ordersCollection.deleteOne(query);
+    res.send(result);
+    });
   } finally {
   }
 }
